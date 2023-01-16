9aa392f36ac9e350fca2280766caf2cc569d2bdb
HugooVer <hugo.vercelloneg@gmail.com>
2023-01-16 22:25:13 +0100

    .h

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,24 @@
 /*   By: hvercell <hvercell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 16:38:55 by hvercell          #+#    #+#             */
-/*   Updated: 2023/01/16 18:34:15 by hvercell         ###   ########.fr       */
+/*   Updated: 2023/01/16 21:33:38 by hvercell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*get_next_line(int fd)
-{	
+in
 
-	static char		*rest;
+char	*get_next_line(int fd)
+{
+	static char	*rest;
 
 	rest = malloc(sizeof(char) * BUFFER_SIZE + 1);
-		
 }
 
 void	printline(int fd, size_t size, char *buf)
 {
 	size_t		len;
 	char		*t;
-	static char	*rest;
+	static char	rest;
 
 	size = read(fd, buf, BUFFER_SIZE);
 	len = (size_t)ft_memchr_i(buf, '\n', size);
--- get_next_line.h
+++ get_next_line.h
@@ -6,9 +6,13 @@
 /*   By: hvercell <hvercell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 16:39:04 by hvercell          #+#    #+#             */
-/*   Updated: 2022/12/13 18:38:52 by hvercell         ###   ########.fr       */
+/*   Updated: 2023/01/16 21:32:38 by hvercell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 # include <unistd.h>
 # include <stdio.h>
